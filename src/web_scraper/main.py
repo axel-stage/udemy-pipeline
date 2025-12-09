7798c26a252932bbc1d4d2644aa62a56a448fa96
Axel Stage <dataengineer24@proton.me>
2025-12-09 20:00:04 +0100

    dev: rework python web scraper

--- src/web_scraper/main.py
+++ src/web_scraper/main.py
@@ -1,33 +1,28 @@
 import json
 import time
-import boto3
 import logging
-
+import boto3
+from botocore.exceptions import ClientError
 from scraper import Scraper
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# client AWS S3
-s3 = boto3.client('s3')
-
-def upload_data_to_s3(data, bucket, key):
-    s3.put_object(
-        Body=json.dumps(data).encode('utf-8'),
-        Bucket=bucket,
-        Key=key
-    )
-
 def lambda_handler(event, context):
     max_retries = 0
+    logger.info(f"{"#" * 80}")
+    logger.info("Start processing...")
+
+    s3 = boto3.client('s3')
 
     url = event.get("url", "")
-    bucket = event.get("bucket", "")
-    prefix=event.get("prefix", "")
+    bucket_name = event.get("bucket_name", "")
+    upstream_prefix=event.get("upstream_prefix", "")
 
+    logger.info(f"start parsing: {url}")
     udemy = Scraper(url)
     udemy.parse_webpage()
-    logger.info(f"start parsing: {url}")
     data = {
         "url": url,
         "slug": udemy.get_slug(),
@@ -40,9 +35,19 @@ def lambda_handler(event, context):
         "language": udemy.scrape_content("div", {"data-purpose": "lead-course-locale"}),
         "created": str( time.strftime("%Y-%m-%d") ),
     }
-    logger.info(f"Parsed data: {data}")
+    logger.info(f"parsed data: {data}")
 
-    filename = f'udemy_{data["slug"]}_{data["created"]}.json'
-    key =  prefix + filename
-    upload_data_to_s3(data, bucket, key)
-    logger.info(f'File uploaded to S3: {bucket + "/" + key}')
+    file_name = f'udemy_{data["slug"]}_{data["created"]}.json'
+    key =  upstream_prefix + file_name
+    try:
+        s3.put_object(
+            Body=json.dumps(data).encode('utf-8'),
+            Bucket=bucket_name,
+            Key=key
+        )
+        logger.info(f'file uploaded to: {bucket_name + "/" + key}')
+    except ClientError as error:
+      logger.error(error)
+
+    logger.info("Stop processing...")
+    logger.info(f"{"#" * 80}")
